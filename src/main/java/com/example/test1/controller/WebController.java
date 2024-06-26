6eebbd034b7d3fdc2d7768b0abb1ae49f017a46a
debesis27 <shivam27.maske@gmail.com>
2024-06-26 12:03:35 +0530

    Removed uneccesary database connection and folder list view

--- src/main/java/com/example/test1/controller/WebController.java
+++ src/main/java/com/example/test1/controller/WebController.java
@@ -1,24 +1,16 @@
 package com.example.test1.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.example.test1.service.FileScanServiceImpl;
 
 @Controller
 @RequestMapping("/")
 public class WebController {
 
-  @Autowired
-  private FileScanServiceImpl fileScanService;
-
-  @GetMapping("/folders")
-  public String getFolderStructure(Model model) {
-    model.addAttribute("rootFolder", fileScanService.scanConfiguredFolder());
-    return "folder-list";
+  @GetMapping("/")
+  public String redirectToFolderStructureGrid() {
+    return "redirect:/folders-grid";
   }
 
   @GetMapping("/folders-grid")
